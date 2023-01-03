322210512969acd6686f15f27d6130c7e821c451
Mingyueyixi <1093286982@qq.com>
2023-01-03 17:29:43 +0800

    [mod] add night mode check

--- library/src/main/java/com/lu/magic/util/ResUtil.kt
+++ library/src/main/java/com/lu/magic/util/ResUtil.kt
@@ -1,9 +1,12 @@
 package com.lu.magic.util
 
+import android.app.UiModeManager
 import android.content.Context
+import android.content.res.Configuration
 import android.content.res.Resources
 import android.util.TypedValue
 import androidx.annotation.ColorInt
+
 
 object ResUtil {
     @JvmStatic
@@ -13,5 +16,15 @@ object ResUtil {
         val theme: Resources.Theme = context.theme
         theme.resolveAttribute(resId, typedValue, true)
         return typedValue.data
+    }
+
+    fun isAppNightMode(context: Context): Boolean {
+        val uiMode = context.resources.configuration.uiMode
+        return (uiMode and  Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+    }
+
+    fun isSystemNightMode(context: Context) :Boolean{
+        val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+        return uiModeManager.nightMode == UiModeManager.MODE_NIGHT_YES
     }
 }
